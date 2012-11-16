7ea709f30fc8d34d3078f2b9ab26a62211fd6602
Eric Davis <ed@npri.org>
2012-11-16 15:32:51 -0800

    Split last_modified and cache_control into separate modules

---
+++ last_modified/cache_control.py
@@ -0,0 +1,43 @@
+import time
+import calendar
+import datetime
+from django.conf import settings
+from django.core.exceptions import MiddlewareNotUsed, ImproperlyConfigured
+from django.http import HttpResponseNotModified
+from django.utils.http import http_date, parse_http_date_safe
+from django.utils.importlib import import_module
+from django.views.decorators.cache import patch_cache_control
+
+DISABLE_LAST_MODIFIED_MIDDLEWARE = getattr(settings, 'DISABLE_LAST_MODIFIED_MIDDLEWARE', False)
+DISABLE_CACHE_CONTROL_MIDDLEWARE = getattr(settings, 'DISABLE_CACHE_CONTROL_MIDDLEWARE', False)
+LAST_MODIFIED_FUNC               = getattr(settings, 'LAST_MODIFIED_FUNC', None)
+CACHE_MAX_AGE                    = getattr(settings, 'CACHE_MAX_AGE', 3600) # one hour
+CACHE_SHARED_MAX_AGE             = getattr(settings, 'CACHE_SHARED_MAX_AGE', CACHE_MAX_AGE)
+
+class CacheControlMiddleware(object):
+    def __init__(self):
+        if DISABLE_CACHE_CONTROL_MIDDLEWARE:
+            raise MiddlewareNotUsed
+
+    def process_response(self, request, response):
+        """
+        Add 'Cache-Control' and 'Expires' HTTP headers to responses.
+
+        CACHE_MAX_AGE tells the user's browser how many seconds it can
+        serve the cached representation without having to check with the
+        origin server.
+
+        CACHE_SHARED_MAX_AGE is the same as CACHE_MAX_AGE, but it applies
+        to public caches.
+
+        The 'Expires' HTTP header is added for completeness, though
+        'Cache-Control' has been adopted pretty much everywhere and is the
+        HTTP/1.1 standard.
+        """
+        params = dict(
+            max_age = CACHE_MAX_AGE,
+            s_maxage = CACHE_SHARED_MAX_AGE,
+        )
+        patch_cache_control(response, **params)
+        response['Expires'] = http_date(time.time() + CACHE_MAX_AGE)
+        return response
