1692688dc11786cc861c9ae9ca8758d0a6514812
Eric Davis <ed@npri.org>
2012-11-17 13:18:54 -0800

    Add long_description and keywords to package

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup
 
 setup(
-    name="django-last-modified",
-    version="0.1",
-    author="Eric Davis",
-    author_email="ed@npri.org",
-    packages=["last_modified"],
-    url="http://github.com/edavis/django-last-modified",
-    license="MIT",
-    description="Django middleware for adding freshness and validation cache headers",
+    name = "django-last-modified",
+    version = "0.1",
+    author = "Eric Davis",
+    author_email = "ed@npri.org",
+    packages = ["last_modified"],
+    url = "http://github.com/edavis/django-last-modified",
+    license = "MIT",
+    description = "Django middleware for adding freshness and validation cache headers",
+    long_description = open('README.rst').read(),
+    keywords = 'cache, if-modified-since, last-modified, cache-control, expires',
     classifiers = [
         "Framework :: Django",
         "Development Status :: 4 - Beta",
